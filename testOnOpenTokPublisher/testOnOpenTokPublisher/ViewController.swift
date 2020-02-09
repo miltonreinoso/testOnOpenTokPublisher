a0ee65c0a4d94d0da461f5bdc677f7382e80ad40
Milton <reinoso.vm@gmail.com>
2020-02-09 15:52:04 -0300

    First Commit

---
+++ testOnOpenTokPublisher/testOnOpenTokPublisher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testOnOpenTokPublisher
+//
+//  Created by Victor Reinoso on 2/9/20.
+//  Copyright © 2020 Assistcard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
